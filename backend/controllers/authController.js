a518d387c40ffbeac07d03578c8d99f53eae0b17
itsmusmans <itsmusmans@gmail.com>
2022-01-28 16:13:56 +0500

    Update User Password

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -115,6 +115,31 @@ exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
   sendToken(user, 200, res);
 });
 
+//GET currently logged in user details => /api/v1/me
+exports.getUserProfile = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
+
+// Update/Change password => /api/v1/password/update
+exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select("+password");
+
+  // Check previous user password
+  const isMatched = await user.comparePassword(req.body.oldPassword);
+  if (!isMatched) {
+    return next(new ErrorHandler(`Old password is incorrect`, 400));
+  }
+  user.password = req.body.password;
+  await user.save();
+
+  sendToken(user, 200, res);
+});
+
 // Logout user => /api/v1/logout
 
 exports.logout = catchAsyncErrors(async (req, res, next) => {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,12 +8,18 @@ const {
   logout,
   forgotPassword,
   resetPassword,
+  getUserProfile,
+  updatePassword,
 } = require("../controllers/authController");
+
+const { isAuthenticatedUser } = require("../middlerwares/auth");
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").get(logout);
 router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
+router.route("/me").get(isAuthenticatedUser, getUserProfile);
+router.route("/password/update").put(isAuthenticatedUser, updatePassword);
 
 module.exports = router;
